7461dea7052551dac78f2ec9933b8558566691b1
Gabriel Leiva <gaboleiva@192.168.1.7>
2024-04-13 11:38:15 -0400

    Se resuelve el segundo requerimiento

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 // Importamos los paquetes necesarios
 const axios = require('axios');
-const chalk = require('chalk')
-const moment = require('moment')
+//const chalk = require('chalk');
+const moment = require('moment');
 const uuid = require('uuid');
 const _ = require('lodash');
 // Levantamos el puerto
@@ -13,6 +13,9 @@ app.listen(PORT, () => {
     console.log(`Servidor Express iniciado en el puerto ${PORT}`);
 });
 
+const { v4: uuidv4 }  = require('uuid');
+
+const usuarios = [];
 
 app.get('/', async(req, res) => {
 try {
@@ -22,6 +25,18 @@ try {
     const objectData = response.data;
     const userData = objectData.results;
     console.log('Propiedad data del objeto response.data: ', userData);
+
+    userData.forEach((user, index) => {
+        const usuario = {
+          id: uuidv4().slice(0, 6),
+          nombre: user.name.first,
+          apellido: user.name.last,
+          genero: user.gender,
+        };
+        usuarios.push(usuario);
+        console.log((`id: ${uuidv4().slice(0, 6)} / ${user.name.title} ${user.name.first} ${user.name.last}`));
+      });
+  
 
     // Enviamos con send la informacion para que sea vista por el navegador, maquillada con etiquetas html
     res.send("todo ok");
