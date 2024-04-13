7fcbb44b16c1c722139e80ce1c6e6bcecd66fdf3
Gabriel Leiva <gaboleiva@192.168.1.7>
2024-04-13 11:01:18 -0400

    Se instalan paquetes necesarios y se resuelve el primer requerimiento

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+// Importamos express 
 const express = require("express");
 const app = express();
-
+// Importamos los paquetes necesarios
+const axios = require('axios');
+const chalk = require('chalk')
+const moment = require('moment')
+const uuid = require('uuid');
+const _ = require('lodash');
+// Levantamos el puerto
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Servidor Express iniciado en el puerto ${PORT}`);
+});
+
+
+app.get('/', async(req, res) => {
+try {
+    // 1. El registro de los usuarios debe hacerse con la API Random User usando axios para consultar la data.
+    // Mostramos la API con AXIOS
+    const response = await axios.get('https://randomuser.me/api/?results=11');
+    const objectData = response.data;
+    const userData = objectData.results;
+    console.log('Propiedad data del objeto response.data: ', userData);
+
+    // Enviamos con send la informacion para que sea vista por el navegador, maquillada con etiquetas html
+    res.send("todo ok");
+
+} catch (error) {
+    console.error('Mensaje de error: ', error);
+    res.status(500).json({ error: 'Mensaje de error' });
+}
 });
